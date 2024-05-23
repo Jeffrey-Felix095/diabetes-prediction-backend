cc6901a55125d9f654225ff2ec9a5c7b458db3d5
Jeffrey-Felix095 <74744182+Jeffrey-Felix095@users.noreply.github.com>
2024-05-23 02:15:52 +0000

    update routes

--- routers/predict.py
+++ routers/predict.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas.schemas import Features, Prediction
 import pandas as pd
 from models.rf_model import model
-
 
 router = APIRouter()
 
 @router.post("/predict", response_model=Prediction)
 def predict(features: Features):
+    try:
+        # Validar los datos de entrada
+        if not isinstance(features, Features):
+            raise HTTPException(status_code=400, detail="Los datos de entrada no son válidos")
 
-    data = pd.DataFrame([features.dict()])
-    prediction = model.predict(data)
-    return {"diabetes_prediction": int(prediction[0])}
+        # Convertir los datos en un DataFrame de pandas
+        data = pd.DataFrame([features.dict()])
+
+        # Realizar la predicción
+        prediction = model.predict(data)
+
+        # Retornar la predicción en formato JSON
+        return {"diabetes_prediction": int(prediction[0])}
+
+    except Exception as e:
+        # Manejar cualquier excepción y retornar un error HTTP 500
+        raise HTTPException(status_code=500, detail="Ocurrió un error al procesar la solicitud")
+

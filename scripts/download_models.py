643b889b41f5e7d526689141d9bbb1efef6f1f01
Sonya <sonya.castro.gomez@gmail.com>
2024-05-24 23:52:50 -0500

    Fix: ml_models path

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -25,7 +25,7 @@ def download_blod(blob_name: str) -> str:
     blob_service_client = BlobServiceClient.from_connection_string(BLOB_CONNECTION_STRING)
     blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
     
-    file_path = f"ml_models/{blob_name}"
+    file_path = f"./ml_models/{blob_name}"
     
     with open(file_path, "wb") as download_file:
         download_file.write(blob_client.download_blob().readall())
@@ -34,7 +34,7 @@ def download_blod(blob_name: str) -> str:
 
 
 for model in models.values():
-    file_path = f"ml_models/{model['blob_name']}"
+    file_path = f"./ml_models/{model['blob_name']}"
 
     try:
         model["model"] = joblib.load(file_path) 
